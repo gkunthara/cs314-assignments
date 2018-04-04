e31e603d37b5b504deecbd75ad52bf13d1e6e487
George Kunthara <gkunthara1@gmail.com>
2018-04-03 18:52:08 -0700

    HW7
    finish of HW7

--- index.js
+++ index.js
@@ -1,76 +1,193 @@
-function trueOrFalse() {
+function charSwap(){
+    let input = prompt("Enter input string: ");
+
+    if(input == null){
+        console.log("ok no input")
+    }
+    else if(input.length <= 1){
+        console.log("input not long enough!")
+    }
+    else{
+        let firstChar = input.charAt(0); //first char
+        let lastChar = input.charAt(input.length - 1); //last char
+        let temp = firstChar; //temp variable for swap
+        firstChar = lastChar;
+        lastChar = temp;
+        let newInput = firstChar + input.slice(1, input.length - 1) + lastChar;
+        alert(newInput);
+    }
+}
+
+function hiString(){
+
+    let input = prompt("Enter input string: ");
+
+    if(input == null){
+        console.log("ok no input")
+    }
+    else if(input.length <= 1){
+        console.log("input not long enough!")
+    }
+    // if first two letters begin with 'Hi'
+    else if(input.slice(0,2) == "Hi"){
+        alert(input);
+    }
+    else{
+        let newString = "Hi" + input;
+        alert(newString);
+    }
+}
+
+function threeCharsToFront(){
+
+    let input = prompt("Enter input string: ");
+
+    if(input == null){
+        console.log("ok no input")
+    }
+    else if(input.length < 3 ){
+        console.log("input not long enough!")
+    }
+    else{
+        let lastChars = input.slice(-3); //last three chars
+        let newString = lastChars + input.slice(0, input.length - 3);
+        alert(newString);
+    }
+}
+
+function stringToSentences(){
+    let input = prompt("Enter comma separated list: ");
+    let arr = input.split(",");
+    if(input == null){
+       return console.log("ok no input")
+    }
+    else if(arr.length != 3){
+        alert("incorrect input length!")
+    }
+    else{
+        newArray = arr.map(s => s.trim()); //remove leading whitespaces
+        alert(`My favorite color is ${newArray[0]}, ${newArray[1]} are awesome, and I love ${newArray[2]}`);
+    }
+}
+
+function upperOrLower(){
+
+    let input = prompt("Enter input string: ");
+    if(input == null){
+        alert("ok no input")
+    }
+    else if(input.length < 3){
+        alert(input.toUpperCase());
+    }
+    else{
+        let firstChars = input.slice(0, 3).toLowerCase(); //grab first three chars and make lowercase
+        alert(firstChars + input.slice(3));
+    }
+}
+
+function integerSwap(){
+
+    let input = prompt("Enter comma separated list: ");
+    let arr = input.split(",");
+    if(input == null){
+        alert("ok no input")
+    }
+    else if(arr.length < 2){
+        alert("enter longer list!")
+    }
+    else{
+        let temp = arr[arr.length - 1]; //temp variable is last element
+        arr[arr.length - 1] = arr[0];
+         arr[0] = temp;
+        alert(arr.join());
+    }
+}
+
+function longestString(){
+    let input = prompt("Enter comma separated list: ");
+    let arr = input.split(",");
+    let newArray = arr.map(s => s.trim());
+    if(input == null){
+        alert("ok no input")
+    }
+    else{
+        longest = newArray[0];
+
+        //loop through list comparing two elements for which is longer
+        for(i = 1; i < newArray.length; i++){
+            if(longest.length < newArray[i].length){
+                longest = newArray[i];
+            }
+        }
+        alert(longest);
+    }
+}
+
+function largestEvenNumber(){
+    let input = prompt("Enter comma separated list: ");
+    let arr = input.split(",");
+    let newArray = arr.map(s => Number(s)); //convert strings to numbers
+    newArray = newArray.filter(n => n % 2 == 0); //filter to only have even numbers
+    largest = newArray[0];
+
+    //loop through comparing two elements and choosing which is larger
+    for(i = 1; i < newArray.length; i++){
+        if(largest < newArray[i]){
+            largest = newArray[i];
+        }
+    }
+    alert(largest);
+}
+
+function currentDayTime(){
+    let today = new Date();
+    let hour = today.getHours();
+    let meridian = ""
+
+    //if hour is in the PM, convert to normal time
+    if(hour > 12){
+        hour = hour - 12
+        meridian = "PM"
+    }
+    else{
+        hour = hour
+        meridian = "AM"
+    }
+    let minute = today.getMinutes();
+    let msg = hour + ":" + minute;
+    day = today.getDay();
+    weekday = ""
     
-    if(confirm("True or False?")){
-        console.log("true")
+    //convert number of day given to actual name of day
+    switch(day){
+        case 1:
+            weekday = "Monday"
+            break;
+        case 2:
+            weekday = "Tuesday"
+            break;
+        case 3:
+            weekday = "Wednesday"
+            break;
+        case 4:
+            weekday = "Thursday"
+            break;
+        case 5:
+            weekday = "Friday"
+            break;
+        case 6:
+            weekday = "Saturday"
+            break;
+        case 7:
+            weekday = "Sunday"
+            break;
     }
-    else{
-        console.log("false")
-    }
+
+    alert("Today is " + weekday + '.' + '\n' + "It is " + msg + meridian + '.')
 }
 
-function trueOrFalseTernary(){
-    confirm("True or False?") ? console.log("true") : console.log("false");
+function unlimited(...args){
+    alert("unlimited Arguments: " + args.join(""))
 }
 
-function enterNumber(){
-    let input = prompt("Enter a number");
-    if(input === null)
-    {
-        alert("ok...")
-    }
-    else{
-        input = parseInt(input * 1);
-    }
-    if(isNaN(input)){
-        alert("whoaaa that's not a number")
-    }
-    
-    else if(input == 0){
-        alert("fine, don't input anything")
-
-    }
-    else if(input === null){
-    }
-    else{
-        alert("it's a number!")
-    }
-        
-}
-
-function makeObj1(first, middle, last){
-
-        this.firstName =  first,
-        this.middleName = middle,
-        this.lastName = last,
-        this.fullName = function() {return this.firstName + " " + this.middleName + " " + this.lastName;}
-
-}
-
-let person = new makeObj1("George", "Joseph", "Kunthara");
-console.log("first person " + person);
-console.log("first person's last name is: " + person.lastName);
-
-
-let person2 = Object.assign({age: 21}, person);
-console.log("second person" + person2);
-console.log("second person's last name is: " + person2.lastName);
-console.log("second person's age: " + person2.age);
-
-
-// function myFunction(str1, str2, str3){
-//     str3 = str3 || "";
-//     str = str1 + str2 + str3;
-//     return str
-// }
-
-// let str = function(str1, str2, str3) {
-//     str3 = str3 || "";
-//     return str1 + str2 + str3;
-// }
-
-let str = (str1, str2, str3) => { return str1 + str2 + (str3 || "")}
-
-console.log(str("bee", "boo", "bop"))
-// console.log(myFunction("bee", "boo"))
-
-
+unlimited("psdf", "sdfdsf", "tyuyt", "fgnbfgn")
